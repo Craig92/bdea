9e41e44e33e979e05b1c6d39e4b2632e1ab5f0ae
thorsten <thorsten.foehringer@outlook.de>
2018-05-17 18:23:03 +0200

    CassandraCluster Klasse mit leeren Methoden erstellt

---
+++ BDEA_SS18_UEBUNG2/src/main/java/database/MyCassandraCluster.java
@@ -0,0 +1,39 @@
+package database;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+
+public class MyCassandraCluster {
+
+	private Cluster cluster;
+
+	MyCassandraCluster() {
+		cluster = Cluster.builder().addContactPoint(" hoppy.informatik.hs-mannheim.de ").withPort(9042).build();
+	}
+
+	public void writeDFIntoDatabase() {
+		Session session = cluster.connect();
+		session.execute("");
+		session.close();
+	}
+
+	public void writeTFIntoDatabase() {
+		Session session = cluster.connect();
+		session.execute("");
+		session.close();
+	}
+
+	public void readDFFromDatabase() {
+		Session session = cluster.connect();
+		ResultSet results = session.execute("");
+		session.close();
+	}
+
+	public void readTFFromDatabase() {
+		Session session = cluster.connect();
+		ResultSet results = session.execute("");
+		session.close();
+	}
+
+}
