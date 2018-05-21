8e679410269eaeb44fbe3bd09709cea915308b2f
Jonas <1428107@stud.hs-mannheim.de>
2018-05-21 21:40:44 +0200

    Cassandra anbindung

--- BDEA_SS18_UEBUNG2/src/main/java/database/MyCassandraCluster.java
+++ BDEA_SS18_UEBUNG2/src/main/java/database/MyCassandraCluster.java
@@ -2,38 +2,68 @@ package database;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.sun.tools.javac.util.List;
 
 public class MyCassandraCluster {
 
 	private Cluster cluster;
 
-	MyCassandraCluster() {
+	public MyCassandraCluster() {
 		cluster = Cluster.builder().addContactPoint("hoppy.informatik.hs-mannheim.de").withPort(9042).build();
 	}
 
-	public void writeDFIntoDatabase() {
+	public void writeDFIntoDatabase(String word, int df) {
 		Session session = cluster.connect();
-		session.execute("");
+		session.execute("INSERT INTO jweis.df (word,value) " 
+				+ "VALUES('" + word + "'," + df + ");");
+		
 		session.close();
 	}
 
-	public void writeTFIntoDatabase() {
+	public void writeTFIntoDatabase(String word, int tf) {
 		Session session = cluster.connect();
-		session.execute("");
+		session.execute("INSERT INTO jweis.tf (word,value) " 
+				+ "VALUES('" + word + "'," + tf + ");");
+		
 		session.close();
 	}
 
-	public void readDFFromDatabase() {
+	public int readDFFromDatabase(String word) {
+		int ret = 0;
 		Session session = cluster.connect();
-		ResultSet results = session.execute("");
+		ResultSet results = session.execute("SELECT * FROM jweis.df " 
+				+ "where word = '" + word + "';");
 		session.close();
+		for (Row row : results) {
+			String number = row.toString().split(",")[1].split("]")[0];
+			ret = Integer.parseInt(number.substring(1, number.length()));
+			System.out.println(ret);
+		}
+		return ret;
 	}
 
-	public void readTFFromDatabase() {
+	public int readTFFromDatabase(String word) {
+		int ret = 0;
 		Session session = cluster.connect();
-		ResultSet results = session.execute("");
+		ResultSet results = session.execute("SELECT * FROM jweis.tf " 
+				+ "where word = '" + word + "';");
 		session.close();
+		for (Row row : results) {
+			String number = row.toString().split(",")[1].split("]")[0];
+			ret = Integer.parseInt(number.substring(1, number.length()));
+			System.out.println(ret);
+		}
+		return ret;
 	}
+
+	// TEST
+//	 public static void main(String[] args) {
+//	 MyCassandraCluster c = new MyCassandraCluster();
+//	 c.writeDFIntoDatabase("123", 123);
+//	 c.readDFFromDatabase("test");
+//	 c.readDFFromDatabase("123");
+//	 }
 
 }
