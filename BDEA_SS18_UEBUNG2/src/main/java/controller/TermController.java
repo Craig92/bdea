c52c3552d2ad09fe70a4f3af9fa860abe4c5090e
Jonas <Jonas@Worf>
2018-06-10 23:21:13 +0200

    tfidf changes from laptop

--- BDEA_SS18_UEBUNG2/src/main/java/controller/TermController.java
+++ BDEA_SS18_UEBUNG2/src/main/java/controller/TermController.java
@@ -3,20 +3,23 @@ package controller;
 import java.util.Map.Entry;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.datastax.driver.mapping.annotations.Param;
 
 import database.MyCassandraCluster;
 
 @RestController
 public class TermController {
-	
+
 	private MyCassandraCluster cluster = new MyCassandraCluster();
-	
+
 	@GetMapping("/significallyTerm")
 	public String getSignifikantTerm() {
 
 		String result = "";
-		for (Entry<String, String> entry :cluster.readDFFromDatabase().entrySet()) {
+		for (Entry<String, String> entry : cluster.readDFFromDatabase().entrySet()) {
 			result += "[" + entry.getKey() + " : " + entry.getValue() + "]\n";
 		}
 		return result;
@@ -26,10 +29,22 @@ public class TermController {
 	public String getMostFrequentTerm() {
 
 		String result = "";
-		for (Entry<String, String> entry :cluster.readTFFromDatabase().entrySet()) {
+		for (Entry<String, String> entry : cluster.readTFFromDatabase().entrySet()) {
 			result += "[" + entry.getKey() + " : " + entry.getValue() + "]\n";
 		}
 		return result;
+	}
+
+	@GetMapping("/tfidf")
+	public String getMostFrequentTerm(@RequestParam("data") String tfName) {
+		System.out.println(tfName);
+		String result = "";
+		int tf = cluster.readTFFromDatabase(tfName);
+		int df = cluster.readDFFromDatabase(tfName.split(" ")[1]);
+		if(df == 0)
+			df = 1;
+		double tfid = tf * Math.log(10 / df);
+		return result + tfid;
 	}
 
 }
