a525268097041983336e2dda0d896cc21e768b51
thorsten <thorsten.foehringer@outlook.de>
2018-06-16 18:46:08 +0200

    Überarbeitetet (hyper)loglog

--- BDEA_SS18_UEBUNG2/src/main/java/hyperLogLog/HyperLogLog.java
+++ BDEA_SS18_UEBUNG2/src/main/java/hyperLogLog/HyperLogLog.java
@@ -1,87 +1,126 @@
 package hyperLogLog;
 
+import org.junit.Test;
+
 public class HyperLogLog {
+
+	public static int[] buckets = new int[1024];
+
+	@Test
+	public void test() {
+		String[] test = new String[] { "1101100011100110", "1111100011001100", "0000110001100110", "1101100001111010" };
+		for (String input : test) {
+			buckets[getBucketNr(input)] = getNumberOfZero(input);
+			System.out.println("Bucket " + getBucketNr(input) + " : " + getNumberOfZero(input));
+		}
+
+		System.out.println("HyperLogLog: E = " + hyperLogLogFunction() / buckets.length + " %");
+		System.out.println("LogLog:      E = " + logLogFunction() / buckets.length + " %");
+
+	}
 
 	public static void main(String args[]) {
 
 		String input;
-		int[] buckets = new int[64];
 
-		for (int i = 0; i < 10; i++) {
+		// befüllen der Buckets mit zufälligen Werten
+		for (int i = 0; i < 1000; i++) {
 			input = generateRandomHash();
-			System.out.println(input);
-			int bucketNr = getBucketNr(input);
-			int nrZeros = getNrZeros(input);
-			buckets[bucketNr] = nrZeros;
+			buckets[getBucketNr(input)] = getNumberOfZero(input);
+			System.out.println("Bucket " + getBucketNr(input) + " : " + getNumberOfZero(input));
 		}
 
-		double sumLL = 0;
-		double sumHLL = 0;
-
-		for (int i = 0; i < 64; i++) {
-			System.out.println(buckets[i]);
-			sumLL += buckets[i];
-			sumHLL += Math.pow(2, -buckets[i]);
-		}
-
-		System.out.println(sumLL);
-		System.out.println(sumHLL);
-
-		double valueLL = 0.783 * 64 * Math.pow(2, (1 / 64) * sumLL);
-		double valueHLL = (0.783 * 64 * 64) / sumHLL;
-
-		System.out.println(valueLL);
-		System.out.println(valueHLL);
+		// Ergebnisse
+		System.out.println("HyperLogLog: E = " + hyperLogLogFunction() / buckets.length + " %");
+		System.out.println("LogLog:      E = " + logLogFunction() / buckets.length + " %");
 
 	}
 
 	/**
+	 * Generate a random hash with 16 bits
 	 * 
-	 * @return
+	 * @return the hashcode
 	 */
 	public static String generateRandomHash() {
 		String ret = "";
-		for (int i = 0; i < 32; i++) {
+		for (int i = 0; i < 16; i++) {
 			if (Math.random() >= 0.5)
 				ret += "1";
 			else
 				ret += "0";
 		}
+		System.out.println("Hash: " + ret);
 		return ret;
 	}
 
 	/**
+	 * get the bucket number
 	 * 
 	 * @param hash
-	 * @return
-	 */
-	public static int getNrZeros(String hash) {
-		char letter;
-		for (int i = 6; i < 32; i++) {
-			letter = hash.charAt(i);
-			if (letter == '1')
-				return (i - 6);
-		}
-		return 0;
-	}
-
-	/**
-	 * 
-	 * @param hash
-	 * @return
+	 *            the handed hash
+	 * @return the numer of the bucket
 	 */
 	public static int getBucketNr(String hash) {
-		String bucketBinary = hash.substring(0, 5);
-		return Integer.parseInt(bucketBinary, 2);
+		return Integer.parseInt(hash.substring(0, 6), 2);
 	}
 
 	/**
+	 * get the numbers of zero int the hash
 	 * 
-	 * @param word
-	 * @return
+	 * @param hash
+	 *            the handed hash
+	 * @return number of zero
 	 */
-	public static String hashfunction(String word) {
-		String hash;
-		return null;
+	public static int getNumberOfZero(String hash) {
+
+		int result = 0;
+		char letter;
+		for (int i = 6; i != 16; i++) {
+			letter = hash.charAt(i);
+			if (letter == '1') {
+				return result;
+			} else {
+				result++;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Calculate the hyperloglog result
+	 * 
+	 * @return the result
+	 */
+	public static double hyperLogLogFunction() {
+
+		double am = 0.783;
+		double m = buckets.length;
+		double sum = 0;
+
+		for (int i = 0; i < buckets.length; i++) {
+			sum += Math.pow(2, -buckets[i]);
+		}
+
+		return (am * (m * m)) / sum;
+
+	}
+
+	/**
+	 * Calculate the loglog result
+	 * 
+	 * @return the result
+	 */
+	public static double logLogFunction() {
+
+		double am = 0.783;
+		double m = buckets.length;
+		double sum = 0;
+
+		for (int i = 0; i < buckets.length; i++) {
+			sum += buckets[i];
+		}
+
+		return am * (m * (Math.pow(2, ((1 / m) * sum))));
+
 	}
 }
