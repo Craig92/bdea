99018c55e34f822515941d9c02b51404073d5e93
Jonas <Jonas@141.19.157.81>
2018-06-18 12:31:07 +0200

    working hll (?)

--- BDEA_SS18_UEBUNG2/src/main/java/hyperLogLog/HLL.java
+++ BDEA_SS18_UEBUNG2/src/main/java/hyperLogLog/HLL.java
@@ -1,65 +1,79 @@
 package hyperLogLog;
 
+import java.text.DecimalFormat;
+
 public class HLL {
 
-	static int numberOfevents = 160000;
+	static int numberOfevents = 16000;
 	static int bucketSize = 1024;
 	static int hashLength = 32;
 
-	public static int count;
+	public static int count = 0;
 
 	public static void main(String args[]) {
-		while (doAlgorithms())
-			;
+		while (doAlgorithms()) {
+
+		}
 	}
 
 	public static boolean doAlgorithms() {
-		count = 0;
+		System.out.println();
+		System.out.println("run is " + ++count);
 		String input;
 
 		int[] buckets = new int[bucketSize];
+		int[] bucketsHLL = new int[bucketSize];
 
 		for (int i = 0; i < numberOfevents; i++) {
 			input = generateRandomHash();
-			// input = hashfunction(input);
-			// System.out.println(input);
 
 			int bucketNr = getBucketNr(input);
 
+			// LogLog
 			int nrZeros = getNrZeros(input);
 
-			buckets[bucketNr] += nrZeros;
+			if (nrZeros > buckets[bucketNr])
+				buckets[bucketNr] = nrZeros;
+
+			// HyperLogLog
+			int nrZerosHLL = getNrZerosHll(input);
+
+			if (nrZerosHLL > bucketsHLL[bucketNr])
+				bucketsHLL[bucketNr] = nrZerosHLL;
 
 		}
-		System.out.println("count is " + count);
+
 		double sumLL = 0;
 
 		double sumHLL = 0;
 
-		for (int i = 0; i < 64; i++) {
+		for (int i = 0; i < bucketSize; i++) {
 			sumLL += buckets[i];
 
-			sumHLL += Math.pow(2, -buckets[i]);
+			sumHLL += Math.pow(2, -bucketsHLL[i]);
 		}
-		System.out.println();
-		System.out.println("testvalue " + Math.pow(2, sumLL / bucketSize));
-		System.out.println();
+
+		// output
+		System.out.println("Number of events " + numberOfevents);
 		System.out.println("sum for loglog: " + sumLL);
 
 		System.out.println("sum for hll: " + sumHLL);
 
-		double valueLL = 0.783 
-				//* bucketSize 
-				* Math.pow(2, sumLL / bucketSize);
+		double valueLL = 0.783 * bucketSize * Math.pow(2, sumLL / bucketSize);
 
 		double valueHLL = (0.783 * bucketSize * bucketSize) / sumHLL;
 
 		System.out.println("e value of ll: " + valueLL);
 		System.out.println("e value of hll: " + valueHLL);
-		
-		if (numberOfevents / 3 < Math.abs(numberOfevents - valueHLL)
-				&& numberOfevents / 3 < Math.abs(numberOfevents- valueLL)
-				)
+
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		System.out.println("Error ll: " + df.format(((valueLL / numberOfevents) - 1) * 100) + "%");
+		System.out.println("Error hll: " + df.format(((valueHLL / numberOfevents) - 1) * 100) + "%");
+
+		// repeat if more than 10% error
+		if (numberOfevents / 20 < Math.abs(numberOfevents - valueHLL)
+				|| numberOfevents / 20 < Math.abs(numberOfevents - valueLL))
 			return true;
 
 		return false;
@@ -89,21 +103,28 @@ public class HLL {
 
 	public static int getNrZeros(String hash) {
 		char letter;
-		for (int i = 8; i < hashLength; i++) {
+		for (int i = 10; i < hashLength; i++) {
 			letter = hash.charAt(i);
 			if (letter == '1') {
-				return (i - 8);
+				return (i - 10);
 			}
+		}
+		return 0;
+	}
 
-			// -5 for loglog
+	public static int getNrZerosHll(String hash) {
+		char letter;
+		for (int i = 10; i < hashLength; i++) {
+			letter = hash.charAt(i);
+			if (letter == '1') {
+				return (i - 9);
+			}
 		}
 		return 0;
 	}
 
 	public static int getBucketNr(String hash) {
 		String bucketBinary = hash.substring(0, 10);
-		if (Integer.parseInt(bucketBinary, 2) % 2 == 0)
-			count++;
 
 		return Integer.parseInt(bucketBinary, 2);
 	}
