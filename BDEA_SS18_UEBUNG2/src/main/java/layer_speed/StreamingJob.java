81e7b610a066b4154788ee4b08318ed7a4f73bfa
thorsten <thorsten.foehringer@outlook.de>
2018-06-19 18:35:32 +0200

    Anpassungen

--- BDEA_SS18_UEBUNG2/src/main/java/layer_speed/StreamingJob.java
+++ BDEA_SS18_UEBUNG2/src/main/java/layer_speed/StreamingJob.java
@@ -3,7 +3,7 @@ package layer_speed;
 import java.util.Properties;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -33,29 +33,23 @@ public class StreamingJob {
 			DataStream<String> messageStream = env
 					.addSource(new FlinkKafkaConsumer011<>("jweis", new SimpleStringSchema(), props));
 
-			DataStream<Tuple1<String>> dataStream = messageStream
+			DataStream<Tuple2<String, Integer>> dataStream = messageStream
 
-					.flatMap(new FlatMapFunction<String, Tuple1<String>>() {
+					.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
 						private static final long serialVersionUID = 1L;
 
 						@Override
-						public void flatMap(String line, Collector<Tuple1<String>> out) throws Exception {
+						public void flatMap(String line, Collector<Tuple2<String, Integer>> out) throws Exception {
 							String[] words = line.replaceAll("[^a-zA-ZäöüÄÖÜß ]", "").split("\\s+");
 							for (String word : words) {
 								System.out.println(word);
-								out.collect(new Tuple1<String>(filename + " " + word.toLowerCase()));
+								out.collect(new Tuple2<String, Integer>(filename + " " + word, 1));
 							}
 						}
 					}).keyBy(0).timeWindowAll(Time.seconds(30)).sum(1);
-			// ohne die shuffle FUnktion wird nur das erste Wort des Textes hinzugefügt
-			// mit Shuffle werden zumindest teile hinzugefügt und gezählt
-
-			// Zusatz für DF können wir .keyBy(0) verwenden
-			// bei diesem aufruf wird jedes wort 1 mal gezählt
-			;
 
 			dataStream.print();
-			CassandraSink.addSink(dataStream).setQuery("UPDATE jweis.tf SET tf = tf + 1 WHERE word = ?;")
+			CassandraSink.addSink(dataStream).setQuery("INSERT INTO jweis.tf(word, tf) values (?, ?);")
 					.setHost("hoppy.informatik.hs-mannheim.de", 9042).build();
 			env.execute();
 
